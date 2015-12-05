37e3fa4f99e577b6e67f756b14df27e66c880a4a
Modulus <thejohnskauge@gmail.com>
2015-12-05 18:49:00 +0100

    Cleanup on aisle 5

--- target/modifiers.js
+++
@@ -1,9 +0,0 @@
-var Animal2 = (function () {
-    function Animal2(theName) {
-        this.name = theName;
-    }
-    Animal2.prototype.move = function (meters) {
-        alert(this.name + " moved " + meters + "m.");
-    };
-    return Animal2;
-})();
